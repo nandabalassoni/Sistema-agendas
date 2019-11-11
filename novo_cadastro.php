f07127f14daab491cb4bc55a55eb685a3a1512bf
Maria Fernanda Balassoni <33336355+nandabalassoni@users.noreply.github.com>
2019-11-11 11:17:12 -0300

    Ajustes na query do banco

--- novo_cadastro.php
+++ novo_cadastro.php
@@ -2,7 +2,7 @@
 
 	// FAZENDO A LIGAÇÃO COM O BANCO DE DADOS. SE TIVER ALGUM ERRO DE CONEXÃO APARECE A MENSAGEM "NÃO FOI POSSÍVEL CONECTAR"
   
-  	$connect = mysqli_connect("localhost", "root", "rootroot", "sistema_agendas") or die ("Não foi possível conectar".mysqli_connect_error());
+  	$connect = mysqli_connect("localhost", "user", "password", "sistema_agendas") or die ("Não foi possível conectar".mysqli_connect_error());
 
   	// PEGANDO OS DADOS DO BANCO E FAZENDO A VALIDAÇÃO NECESSÁRIA
 

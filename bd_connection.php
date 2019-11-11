26bef26ccfe762236e73350796ff9a1d61b06e33
Maria Fernanda Balassoni <33336355+nandabalassoni@users.noreply.github.com>
2019-11-11 11:15:00 -0300

    excluindo arquivo que não é necessário

--- bd_connection.php
+++ bd_connection.php
@@ -1,16 +1,1 @@
-<?php
 
-	$bd = mysqli_connect("localhost", "root", "rootroot", "bd.scg") or die ("Não foi possível conectar".mysqli_connect_error());
-
-	$connect = "INSERT INTO bd_scg (user, senha) VALUES ('$_POST[user]', '$_POST[senha]')";
-
-	if(mysqli_query($connect, $bd)){
-		echo "Dados inseridos";
-	} else{
-		echo "Erro ao adicionar dados";
-	}
-
-	mysqli_close($connect);
-
-
-?>

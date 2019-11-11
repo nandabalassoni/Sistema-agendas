3bd037d933b337f435b3c8a4076facbabdd7211a
Maria Fernanda Balassoni <33336355+nandabalassoni@users.noreply.github.com>
2019-11-11 11:08:57 -0300

    ajustando nome do banco

--- viewtable.php
+++ viewtable.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con = mysqli_connect("localhost", "root", "rootroot", "sistema_agendas") or die ("Não foi possível conectar".mysqli_connect_error());
+	$con = mysqli_connect("localhost", "", "", "sistema_agendas") or die ("Não foi possível conectar".mysqli_connect_error());
 
     // mysqli_close($con);
 ?>
